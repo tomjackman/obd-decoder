144fd96289e981af63c3d7bb70a14403497b24b1
Tom Jackman <tom.dls@hotmail.com>
2016-06-18 16:15:21 +0100

    add test

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,3 @@ exports.hexToDecimal = function(hexString)
 {
   return parseInt(hexString, 16);
 }
-
-
-
-};
---
+++ routes/test.js
@@ -0,0 +1,5 @@
+var decoder = require('./index');
+
+var decNum = decoder.hexToDecimal("7ff");
+
+console.log(decNum);
