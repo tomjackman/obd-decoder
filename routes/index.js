8692605ff543a5b90f00215b0ff36233b23091fb
Tom Jackman <tom.dls@hotmail.com>
2016-06-18 16:06:02 +0100

    add hex decoding

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+/*
+  Converts a hexidecimal string to decimal
+  @param hexString {String} The hexidecimal string to convert to decimal.
+*/
+exports.hexToDecimal = function(hexString)
+{
+  return parseInt(hexString, 16);
+}
 
-module.exports = router;
+
+
+};
