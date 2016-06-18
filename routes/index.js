b209aa2c3009e6769e7072d1000be68950c49660
Tom Jackman <tom.dls@hotmail.com>
2016-06-18 16:15:48 +0100

    add truncate feature

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 
 /*
-  Converts a hexidecimal string to decimal
+  Converts a hexidecimal string to decimal.
   @param hexString {String} The hexidecimal string to convert to decimal.
 */
 exports.hexToDecimal = function(hexString)
 {
   return parseInt(hexString, 16);
+}
+
+/*
+  Truncates a ODB-II response.
+  @param hexString {String} The hexidecimal string to truncate.
+*/
+exports.truncate = function(hexString)
+{
+  return hexString.replace(/\s/g, '');
 }
