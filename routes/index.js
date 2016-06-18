bb8ca5de5697e81c4701b7eee59623232f3f5c1d
Tom Jackman <tom.dls@hotmail.com>
2016-06-18 19:34:29 +0100

    add get pid info feature

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var PARAMETER_IDS = require('../config/parameterIDs.json');
+var TROUBLE_CODES = require('../config/troubleCodes.json');
 
 /*
   Converts a hexidecimal string to decimal.
@@ -6,6 +8,7 @@ var express = require('express');
 */
 exports.hexToDecimal = function(hexString)
 {
+  hexString = exports.truncate(hexString);
   return parseInt(hexString, 16);
 }
 
@@ -16,4 +19,29 @@ exports.hexToDecimal = function(hexString)
 exports.truncate = function(hexString)
 {
   return hexString.replace(/\s/g, '');
+}
+
+/*
+  Identifies the parameter id based on the response from the vehicle.
+  @param hexString {String} The response string to try and identify.
+*/
+exports.getParameterID = function(hexString)
+{
+  hexString = exports.truncate(hexString);
+  var mode = hexString.substring(1);
+  var pid = exports.hexToDecimal(hexString.substring(2,3));
+
+  if(mode === 1 || mode === 9){
+    return "result" : {
+      "status": "valid",
+      "pid": PARAMETER_IDS[mode][pid].pid,
+    	"bytes": PARAMETER_IDS[mode][pid].bytes,
+    	"description": PARAMETER_IDS[mode][pid].description
+    };
+  }
+  else{
+    return "result" : {
+      "status": "invalid"
+    };
+  }
 }
